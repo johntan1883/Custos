0b45e03445b35c79a2b049730459f06d03fa56cc
Immersian <kurtiskhoo@gmail.com>
2024-03-26 01:02:08 +0800

    Added game over and pause
    Poopy head

---
+++ Assets/Buttons.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenuCanvas;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+
+        }
+    }
+    void Stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Player.SetActive(false);
+        Time.timeScale = 0.0f;
+        Paused = true;
+    }
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Player.SetActive(true);
+        Time.timeScale = 1f;
+        Paused = false;
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Should Quit the game");
+
+    }
+    public void Resume()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Player.SetActive(true);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
